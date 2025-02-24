91f0302e71d0a9709f19fc9669f1eb518a23b62a
ToastnotAI <ryanchristie03@gmail.com>
2025-02-24 14:10:00 +0000

    Cool Java script

--- aboutme.php
+++ aboutme.php
@@ -16,5 +16,24 @@
             echo "<span style='color:#$hex'>$letter</span>";
         }
         ?>
+        <p id = "javaDemo"></p>
+        <script>
+            function randomColor() {
+                var message = "Hello World! (java)";
+                var newMessage = "";
+                for (var i = 0; i < message.length; i++) {
+                    var color = Math.floor(Math.random() * 16777215).toString(16);
+                    while (color.length < 6) {
+                        color = "0" + color; // Ensure the color code is 6 digits long
+                    }
+                    var letter = message.charAt(i);
+                    newMessage += "<span style='color:#" + color + "'>" + letter + "</span>";
+                }
+                document.getElementById("javaDemo").innerHTML = newMessage;
+            }
+
+            // Call the function every 1000 milliseconds
+            setInterval(randomColor, 1000);
+        </script>
     </body>
 </html>
